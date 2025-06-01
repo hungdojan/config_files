517a4bd12ac3995232a698ddf257b4f882926d57
Hung Do <hungdojan@gmail.com>
2025-06-01 10:47:19 +0200

    LSP config refactor

--- nvim/lua/plugins/lsp/lspConfig.lua
+++ nvim/lua/plugins/lsp/lspConfig.lua
@@ -1,3 +1,88 @@
+local settingTable = {
+	lua_ls = {
+		settings = {
+			Lua = {
+				runtime = {
+					path = {
+						"?.lua",
+						"?/init.lua",
+						vim.fn.expand("~/.luarocks/share/lua/5.4/?.lua"),
+						vim.fn.expand("~/.luarocks/share/lua/5.4/?/init.lua"),
+						"/usr/share/5.4/?.lua",
+						"/usr/share/lua/5.4/?/init.lua",
+					},
+				},
+				workspace = {
+					library = {
+						vim.fn.expand("~/.luarocks/share/lua/5.4"),
+						"/usr/share/lua/5.4",
+					},
+				},
+				hint = { enable = true },
+				telemetry = {
+					enable = false,
+				},
+			},
+		},
+	},
+	ts_ls = {
+		javascript = {
+			inlayHints = {
+				includeInlayEnumMemberValueHints = true,
+				includeInlayFunctionLikeReturnTypeHints = true,
+				includeInlayFunctionParameterTypeHints = true,
+				includeInlayParameterNameHints = "all",
+				includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+				includeInlayPropertyDeclarationTypeHints = true,
+				includeInlayVariableTypeHints = true,
+				includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+			},
+		},
+		typescript = {
+			inlayHints = {
+				includeInlayEnumMemberValueHints = true,
+				includeInlayFunctionLikeReturnTypeHints = true,
+				includeInlayFunctionParameterTypeHints = true,
+				includeInlayParameterNameHints = "all",
+				includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+				includeInlayPropertyDeclarationTypeHints = true,
+				includeInlayVariableTypeHints = true,
+				includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+			},
+		},
+	},
+	ccls = {
+		init_options = {
+			cache = {
+				directory = "/tmp/ccls",
+			},
+			clang = {
+				extraArgs = { "-Wunused", "-Wunused-parameter", "-Wall" },
+				excludeArgs = {},
+			},
+		},
+		single_file_support = true,
+	},
+	yamlls = {
+		settings = {
+			redhat = {
+				telemetry = {
+					enable = false,
+				},
+			},
+			yaml = {
+				format = {
+					enable = true,
+				},
+			},
+		},
+	},
+	gopls = {},
+	rust_analyzer = {},
+	bashls = {},
+	pyright = {},
+}
+
 local function newLspConfig()
 	vim.lsp.enable({
 		"ccls",
@@ -16,90 +101,15 @@ local function newLspConfig()
 
 	-- configs
 	-- requires `ccls`
-	vim.lsp.config("ccls", {
-		init_options = {
-			cache = {
-				directory = "/tmp/ccls",
-			},
-			clang = {
-				extraArgs = { "-Wunused", "-Wunused-parameter", "-Wall" },
-				excludeArgs = {},
-			},
-		},
-		single_file_support = true,
-	})
-
+	vim.lsp.config("ccls", settingTable.ccls)
 	-- requires `lua-language-server`
-	vim.lsp.config("lua_ls", {
-		settings = {
-			Lua = {
-				runtime = {
-					path = {
-						"?.lua",
-						"?/init.lua",
-						vim.fn.expand("~/.luarocks/share/lua/5.4/?.lua"),
-						vim.fn.expand("~/.luarocks/share/lua/5.4/?/init.lua"),
-						"/usr/share/5.4/?.lua",
-						"/usr/share/lua/5.4/?/init.lua",
-					},
-				},
-				workspace = {
-					library = {
-						vim.fn.expand("~/.luarocks/share/lua/5.4"),
-						"/usr/share/lua/5.4",
-					},
-				},
-				hint = { enable = true },
-				telemetry = {
-					enable = false,
-				},
-			},
-		},
-	})
+	vim.lsp.config("lua_ls", settingTable.lua_ls)
 
 	-- requires `typescript-language-server`
-	vim.lsp.config("ts_ls", {
-		on_attach = on_attach,
-		javascript = {
-			inlayHints = {
-				includeInlayEnumMemberValueHints = true,
-				includeInlayFunctionLikeReturnTypeHints = true,
-				includeInlayFunctionParameterTypeHints = true,
-				includeInlayParameterNameHints = "all",
-				includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-				includeInlayPropertyDeclarationTypeHints = true,
-				includeInlayVariableTypeHints = true,
-				includeInlayVariableTypeHintsWhenTypeMatchesName = true,
-			},
-		},
-		typescript = {
-			inlayHints = {
-				includeInlayEnumMemberValueHints = true,
-				includeInlayFunctionLikeReturnTypeHints = true,
-				includeInlayFunctionParameterTypeHints = true,
-				includeInlayParameterNameHints = "all",
-				includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-				includeInlayPropertyDeclarationTypeHints = true,
-				includeInlayVariableTypeHints = true,
-				includeInlayVariableTypeHintsWhenTypeMatchesName = true,
-			},
-		},
-	})
+	vim.lsp.config("ts_ls", settingTable.ts_ls)
 
-	vim.lsp.config("yamlls", {
-		settings = {
-			redhat = {
-				telemetry = {
-					enable = false,
-				},
-			},
-			yaml = {
-				format = {
-					enable = true,
-				},
-			},
-		},
-	})
+	vim.lsp.config("yamlls", settingTable.yamlls)
+	vim.diagnostic.config()
 end
 
 local function oldLspConfig()
@@ -114,116 +124,11 @@ local function oldLspConfig()
 	-------------------------------------------------
 	-- LSP setups
 	local lspconfig = require("lspconfig")
-
-	-- requires `ccls`
-	lspconfig.ccls.setup({
-		init_options = {
-			cache = {
-				-- directory = ".ccls-cache";
-				directory = "/tmp/ccls",
-			},
-			clang = {
-				extraArgs = { "-Wunused", "-Wunused-parameter", "-Wall" },
-				excludeArgs = {},
-			},
-		},
-		capabilities = capabilities,
-		on_attach = on_attach,
-		single_file_support = true,
-	})
-
-	-- requires `pyright`
-	lspconfig.pyright.setup({
-		capabilities = capabilities,
-		on_attach = on_attach,
-		-- handlers = {
-		-- 	["textDocument/hover"] = vim.lsp.with(require("setups.pyrightDocsFix").hover, {
-		-- 		-- border = "rounded"
-		-- 		border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-		-- 		title = " |･ω･) ? ",
-		-- 		max_width = 120,
-		-- 		zindex = 500,
-		-- 	}),
-		-- },
-	})
-
-	-- requires `haskell-language-server-wrapper`
-	--          `stylish-haskell`
-	-- lspconfig.hls.setup({
-	-- 	filetype = { "haskell", "lhaskell", "cabal" },
-	-- 	capabilities = capabilities,
-	-- 	on_attach = on_attach,
-	-- })
-
-	-- requires `rust-analyzer`
-	lspconfig.rust_analyzer.setup({
-		on_attach = on_attach,
-	})
-
-	-- requires `bash-language-server`
-	--          `shellcheck`
-	lspconfig.bashls.setup({
-		on_attach = on_attach,
-	})
-
-	-- requires `lua-language-server`
-	lspconfig.lua_ls.setup({
-		on_attach = on_attach,
-		settings = {
-			Lua = {
-				runtime = {
-					path = {
-						"?.lua",
-						"?/init.lua",
-						vim.fn.expand("~/.luarocks/share/lua/5.4/?.lua"),
-						vim.fn.expand("~/.luarocks/share/lua/5.4/?/init.lua"),
-						"/usr/share/5.4/?.lua",
-						"/usr/share/lua/5.4/?/init.lua",
-					},
-				},
-				workspace = {
-					library = {
-						vim.fn.expand("~/.luarocks/share/lua/5.4"),
-						"/usr/share/lua/5.4",
-					},
-				},
-				hint = { enable = true },
-				telemetry = {
-					enable = false,
-				},
-			},
-		},
-	})
-
-	-- requires `typescript-language-server`
-	lspconfig.ts_ls.setup({
-		on_attach = on_attach,
-		javascript = {
-			inlayHints = {
-				includeInlayEnumMemberValueHints = true,
-				includeInlayFunctionLikeReturnTypeHints = true,
-				includeInlayFunctionParameterTypeHints = true,
-				includeInlayParameterNameHints = "all",
-				includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-				includeInlayPropertyDeclarationTypeHints = true,
-				includeInlayVariableTypeHints = true,
-				includeInlayVariableTypeHintsWhenTypeMatchesName = true,
-			},
-		},
-		typescript = {
-			inlayHints = {
-				includeInlayEnumMemberValueHints = true,
-				includeInlayFunctionLikeReturnTypeHints = true,
-				includeInlayFunctionParameterTypeHints = true,
-				includeInlayParameterNameHints = "all",
-				includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-				includeInlayPropertyDeclarationTypeHints = true,
-				includeInlayVariableTypeHints = true,
-				includeInlayVariableTypeHintsWhenTypeMatchesName = true,
-			},
-		},
-	})
-	lspconfig.gopls.setup({})
+	for k, v in pairs(settingTable) do
+		v[on_attach] = on_attach
+		v[capabilities] = capabilities
+		lspconfig[k].setup = v
+	end
 end
 
 return function()
@@ -232,5 +137,12 @@ return function()
 	else
 		oldLspConfig()
 	end
-	vim.diagnostic.config({ virtual_text = true })
+	vim.diagnostic.config({
+		virtual_text = true,
+		underline = true,
+		float = {
+			border = "rounded",
+			source = true,
+		},
+	})
 end
